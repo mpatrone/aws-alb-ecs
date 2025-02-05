bba783ee5de4cdc0e88f27dc6ece770f6590f522
Manuel Patrone <po75558@gmail.com>
2025-02-05 14:50:39 +0000

    vpc complete

--- terraform-code/main.tf
+++ terraform-code/main.tf
@@ -3,6 +3,8 @@
 
 
 module "infra" {
-  source   = "./modules/infra"
-  vpc_cidr = "10.0.0.0/16"
+  source      = "./modules/infra"
+  vpc_cidr    = "10.0.0.0/16"
+  num_subnets = 2
 }
+
--- terraform-code/modules/infra/main.tf
+++ terraform-code/modules/infra/main.tf
@@ -1,7 +1,61 @@
-resource "aws_vpc" "main" {
+locals {
+  azs = data.aws_availability_zones.azsonly.names
+}
+
+
+resource "aws_vpc" "this" {
   cidr_block = var.vpc_cidr
 
   tags = {
     Name = "ecs-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "this" {
+  tags = {
+    Name = "ecs-igw"
+  }
+}
+
+resource "aws_internet_gateway_attachment" "this" {
+  internet_gateway_id = aws_internet_gateway.this.id
+  vpc_id              = aws_vpc.this.id
+}
+
+resource "aws_route_table" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "ecs-rt"
+  }
+}
+
+resource "aws_route" "this" {
+  route_table_id         = aws_route_table.this.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this.id
+}
+
+resource "aws_subnet" "this" {
+  for_each          = { for i in range(var.num_subnets) : "public${i}" => i }
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = cidrsubnet(aws_vpc.this.cidr_block, 8, each.value)
+  availability_zone = local.azs[each.value % length(local.azs)]
+
+  tags = {
+    Name = "ecs-subnet-${each.key}"
+  }
+}
+
+resource "aws_route_table_association" "this" {
+  for_each       = aws_subnet.this
+  subnet_id      = aws_subnet.this[each.key].id
+  route_table_id = aws_route_table.this.id
+}
+
+data "aws_availability_zones" "azsonly" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
   }
 }
